f6f51af48ff8525fe733a23bda50fad5c4bb950b
iranika <7370486+iranika@users.noreply.github.com>
2020-04-30 13:35:34 +0900

    Add: reload module

--- wps.Tests.ps1
+++ wps.Tests.ps1
@@ -1,7 +1,16 @@
 # this script depend Pester.
+param(
+    [switch]$NoReloadModule
+    ,$ModuleName = "wps"
+)
 
 . "$PSScriptRoot\wps.ps1"
 
+if (!$NoReloadModule){
+    # モジュールを再読み込みする
+    Remove-Module $ModuleName -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+    Import-Module "$PSScriptRoot\." -WarningAction SilentlyContinue
+}
 
 
 Describe 'Test wps.ps1' {
